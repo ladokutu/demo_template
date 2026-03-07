c26f86c9b439e780238b0897548b243b794089da
Ladokutu <djon.rikko@gmail.com>
2026-03-07 13:39:45 +0700

    add extra folder

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from "next/script";
 
 export const metadata = {
   title: 'Ladokutu Informasi — Konsultan IT & Custom Software Development Bandung',
@@ -8,8 +9,25 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="id">
-      <body>{children}</body>
+    <html lang="id" suppressHydrationWarning>
+      <body>
+        {children}
+
+        <Script id="tawkto" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/65fcddc2a0c6737bd1237d77/1hphs4g5m';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
+
+      </body>
     </html>
   );
 }
